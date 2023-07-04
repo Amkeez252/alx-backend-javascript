d6cd400bacb72937b29d90421882b1fc6f244623
Amkeez252 <auwalamalik@gmail.com>
2023-07-04 23:03:44 +0000

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,25 @@
-export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
-  return [...set]
-    .filter((el) => el.startsWith(startString))
-    .map((el) => el.slice(startString.length))
-    .join('-');
+function cleanSet(set, startString) {
+    if (!(set instanceof Set) || typeof startString !== 'string') {
+        throw new Error('Invalid arguments: Expected a Set and a string');
+    }
+
+    const valuesArray = [...set];
+    const filteredValues = valuesArray.filter(value => value.startsWith(startString));
+
+    let result = '';
+    for (let i = 0; i < filteredValues.length; i++) {
+        const value = filteredValues[i];
+        if (startString !== '') {
+            const restOfString = value.substring(startString.length);
+            result += restOfString;
+        }
+
+        if (i !== filteredValues.length - 1 && startString !== '') {
+            result += '-';
+        }
+    }
+
+    return result;
 }
-}
-}
+
+export default cleanSet;
