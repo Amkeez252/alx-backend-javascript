fa90ae4d7bdc7107b4a1c16a4376a695bab0f88a
Amkeez252 <auwalamalik@gmail.com>
2023-07-05 09:32:18 +0000

    data manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,25 +1,8 @@
-function cleanSet(set, startString) {
-    if (!(set instanceof Set) || typeof startString !== 'string') {
-        throw new Error('Invalid arguments: Expected a Set and a string');
-    }
-
-    const valuesArray = [...set];
-    const filteredValues = valuesArray.filter(value => value.startsWith(startString));
-
-    let result = '';
-    for (let i = 0; i < filteredValues.length; i++) {
-        const value = filteredValues[i];
-        if (startString !== '') {
-            const restOfString = value.substring(startString.length);
-            result += restOfString;
-        }
-
-        if (i !== filteredValues.length - 1 && startString !== '') {
-            result += '-';
-        }
-    }
-
-    return result;
+export default function cleanSet(set, startString) {
+  let result = '';
+  if (!startString || !startString.length) return result;
+  set.forEach((i) => {
+    if (i && i.startsWith(startString)) result += `${i.slice(startString.length)}-`;
+  });
+  return result.slice(0, result.length - 1);
 }
-
-export default cleanSet;
