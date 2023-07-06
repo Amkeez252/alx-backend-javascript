34e3056b4d7dac48e31197e203deb01f96719def
Auwal Abdulmalik <111090647+Amkeez252@users.noreply.github.com>
2023-07-06 09:58:18 +0000

    Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,46 @@
+interface Teacher{
+    firstName: string;
+    lastName: string;
+    readonly fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any
+}
+
+interface Directors extends Teacher{
+    numberOfReports: number
+}
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    const initial = firstName.charAt(0).toUpperCase();
+    const formattedLastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+
+    return `${initial}. ${formattedLastName}`;
+};
+
+interface StudentClassConstructor {
+    new (firstName: string, lastName: string): StudentClass;
+}
+
+interface StudentClass {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+
+class StudentClass implements StudentClass {
+    constructor(public firstName: string, public lastName: string) {}
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
