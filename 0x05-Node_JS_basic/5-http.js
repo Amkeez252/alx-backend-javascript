321c54b78239853e4b9c8adb7313b6a33fae2481
Amkeez252 <auwalamalik@gmail.com>
2023-08-23 06:11:48 +0000

    Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,67 +1,70 @@
 const http = require('http');
-const fs = require('fs');
+const { readFile } = require('fs');
 
-const readFileAsync = (filename, callback) => {
-  fs.readFile(filename, 'utf8', (error, data) => {
-    if (error) {
-      callback(error, null);
-      return;
-    }
-    callback(null, data);
-  });
-};
+const hostname = '127.0.0.1';
+const port = 1245;
 
-const app = http.createServer((req, res) => {
-  const { url } = req;
-
-  if (url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Holberton School!\n');
-  } else if (url === '/students') {
-    const databaseFile = process.argv[2];
-
-    readFileAsync(databaseFile, (error, data) => {
-      if (error) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error\n');
-        return;
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let length = 0;
+  return new Promise((resolve, reject) => {
+    readFile(fileName, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        let output = '';
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1;
+            const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
+          }
+        }
+        const l = length - 1;
+        output += `Number of students: ${l}\n`;
+        for (const [key, value] of Object.entries(fields)) {
+          if (key !== 'field') {
+            output += `Number of students in ${key}: ${value}. `;
+            output += `List: ${students[key].join(', ')}\n`;
+          }
+        }
+        resolve(output);
       }
-
-      const lines = data.split('\n');
-      const students = lines.filter((line) => line.trim() !== '');
-
-      const totalStudents = students.length;
-      const csStudents = students.filter((student) => student.endsWith(',CS')).length;
-      const sweStudents = students.filter((student) => student.endsWith(',SWE')).length;
-
-      const csStudentList = students
-        .filter((student) => student.endsWith(',CS'))
-        .map((student) => student.split(',')[0])
-        .join(', ');
-
-      const sweStudentList = students
-        .filter((student) => student.endsWith(',SWE'))
-        .map((student) => student.split(',')[0])
-        .join(', ');
-
-      const response = `
-This is the list of our students
-Number of students: ${totalStudents}
-Number of students in CS: ${csStudents}. List: ${csStudentList}
-Number of students in SWE: ${sweStudents}. List: ${sweStudentList}
-`;
-
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end(response);
     });
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found\n');
+  });
+}
+
+const app = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  if (request.url === '/') {
+    response.write('Hello Holberton School!');
+    response.end();
+  }
+  if (request.url === '/students') {
+    response.write('This is the list of our students\n');
+    countStudents(process.argv[2].toString()).then((output) => {
+      const outString = output.slice(0, -1);
+      response.end(outString);
+    }).catch(() => {
+      response.statusCode = 404;
+      response.end('Cannot load the database');
+    });
   }
 });
 
-app.listen(1245, () => {
-  console.log('Server is listening on port 1245');
+app.listen(port, hostname, () => {
 });
 
 module.exports = app;
